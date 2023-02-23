5b7dc7633490eb81b2c2b9eebfbac75e64799822
Mahmoud Gooda <mahmoud.gooda@gmail.com>
2023-02-23 21:20:37 +0000

    1st HW challenge update

--- backend-flask/CMD-script.sh
+++ backend-flask/CMD-script.sh
@@ -1,12 +1,1 @@
-#!/bin/bash
-
-echo setting env. variables
-export BACKEND_URL="*"
-export FRONTEND_URL="*"
-echo Done
-echo Installing requirements
-pip install flask
-pip install flask-cors
-echo Done
-echo run backend-flask
 python3 -m flask run --host=0.0.0.0 --port=4567
