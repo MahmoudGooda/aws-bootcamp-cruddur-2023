3a347d306e584abcf1951a64f6a66c39908d42b7
Mahmoud Gooda <mahmoud.gooda@gmail.com>
2023-03-24 23:22:24 +0000

    add myself to db and create sql query

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -2,6 +2,7 @@
 INSERT INTO public.users (display_name, handle, email, cognito_user_id)
 VALUES
   ('Andrew Brown', 'andrewbrown','andrew@exampro.co', 'MOCK'),
+  ('Mahmoudgooda', 'MGOODA', 'mgooda.gamer@gmail.com', 'MOCK'),
   ('Andrew Bayko', 'bayko','bayko@exampro.co', 'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
