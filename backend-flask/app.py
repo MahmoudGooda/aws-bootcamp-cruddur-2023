f1e710ca783fa085b830143cad537bcf439ce25e
Mahmoud Gooda <mahmoud.gooda@gmail.com>
2023-03-24 23:49:27 +0000

    start DynamoDB implementation and use message group uuid i/o handle

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -134,17 +134,25 @@ def data_message_groups():
   else:
     return model['data'], 200
 
-@app.route("/api/messages/@<string:handle>", methods=['GET'])
-def data_messages(handle):
-  user_sender_handle = 'andrewbrown'
-  user_receiver_handle = request.args.get('user_reciever_handle')
+@app.route("/api/messages/<string:message_group_uuid>", methods=['GET'])
+def data_messages(message_group_uuid):
 
-  model = Messages.run(user_sender_handle=user_sender_handle, user_receiver_handle=user_receiver_handle)
-  if model['errors'] is not None:
-    return model['errors'], 422
-  else:
-    return model['data'], 200
-  return
+  model = Messages.run(
+    message_group_uuid=message_group_uuid,
+    cognito_user_id=cognito_user_id
+    )
+  access_token = extract_access_token(request.headers)
+  try:
+    claims = cognito_jwt_token.verify(access_token)
+    cognito_user_id = claims['sub']
+    if model['errors'] is not None:
+      return model['errors'], 422
+    else:
+      return model['data'], 200
+
+  except TokenVerifyError as e:
+    app.logger.debug(e)
+    return {}, 401
 
 @app.route("/api/messages", methods=['POST','OPTIONS'])
 @cross_origin()
