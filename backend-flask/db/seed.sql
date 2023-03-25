501ddcd6b8b76f6c2307a125646da937252db97d
Mahmoud Gooda <mahmoud.gooda@gmail.com>
2023-03-25 00:28:12 +0000

    sql implementation for new messages

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -3,7 +3,8 @@ INSERT INTO public.users (display_name, handle, email, cognito_user_id)
 VALUES
   ('Andrew Brown', 'andrewbrown','andrew@exampro.co', 'MOCK'),
   ('Mahmoudgooda', 'MGOODA', 'mgooda.gamer@gmail.com', 'MOCK'),
-  ('Andrew Bayko', 'bayko','bayko@exampro.co', 'MOCK');
+  ('Andrew Bayko', 'bayko','bayko@exampro.co', 'MOCK'),
+  ('Londo Mollari', 'lmollari', 'lmollari@centri.com', 'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
